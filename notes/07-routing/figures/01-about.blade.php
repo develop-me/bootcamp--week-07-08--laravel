dd1f82c89854f91f1a4e1360728bfd8e19acb007
Mark Wales <mark@smallhadroncollider.com>
2020-05-01 16:53:21 +0100

    update: adds controllers/routing information

---
+++ notes/07-routing/figures/01-about.blade.php
@@ -0,0 +1,11 @@
+@extends("app")
+
+@section("content")
+  <div class="card">
+    <h2 class="card-header">About</h2>
+    <article class="card-body">
+      <p>This is my amazing blog.</p>
+      <p>It's amazing.</p>
+    </article>
+  </div>
+@endsection
