a9fe23b6a7c8fec6a77c6ad4be5184395e0b304b
Mark Wales <mark@developme.tech>
2020-12-07 12:31:51 +0000

    notes: note about main branch naming

--- notes/20-capistrano/figures/04-production.rb
+++ notes/20-capistrano/figures/04-production.rb
@@ -2,8 +2,9 @@
 # make sure you update the server with your EC2 instance address
 server "ec2-3-8-199-58.eu-west-2.compute.amazonaws.com", user: "ubuntu", roles: %w{app}
 
-# we want to use the master branch of the git repo
-set :branch, "master"
+# we want to use the main branch of the git repo
+# or "master" if you're using the older branch naming convention
+set :branch, "main"
 
 # it should deploy to the following directory
 # update with the appropriate directory on your server
